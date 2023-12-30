7fcfe421fe9680f55fe3a70b9e1602261d053453
zeroche-dev <zeroche@protonmail.com>
2023-12-30 17:59:27 +0100

    install

---
+++ install.sh
@@ -0,0 +1,29 @@
+sudo apt-get install meson \
+pkg-config \
+libgraphene-1.0-dev \
+libgdk-pixbuf2.0-dev \
+cmake \
+libpango1.0-dev \
+gsettings-desktop-schemas-dev \
+libxkbcommon-dev \
+libatk1.0-dev \
+libdbus-1-dev \
+libcolord-dev \
+liblcms2-dev \
+libeis-dev \ 
+libgtk-4-dev \
+libxtst-dev \
+libxkbfile-dev \
+libxkbcommon-x11-dev \
+libx11-xcb-dev \
+libxcb-randr0-dev \
+libxcb-res0-dev \
+libgnome-desktop-4-dev \
+libcanberra-dev \
+libgudev-1.0-dev \
+libgbm-dev \
+libinput-dev \
+libdrm-dev \
+libstartup-notification0-dev \
+libpipewire-0.3-dev \
+gettext
